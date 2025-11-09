6412a8361ca382360d41c50b7e0ebf8df6d2b260
atkyle <kyle-soft@foxmail.com>
2025-11-09 16:04:28 +0800

    Add new feature for searching specified classFilename such as ClassFile.class

--- src/main/java/com/kyle/utils/ClassFinder.java
+++ src/main/java/com/kyle/utils/ClassFinder.java
@@ -15,6 +15,7 @@ import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -26,42 +27,68 @@ public class ClassFinder {
 
 	public static void main(String[] args) {
 		if (args.length < 2) {
-			System.out.println("Usage: java -jar build/libs/Kyle-ClassFinder-1.0.jar [class full name] [jars' folder path]");
+			System.out.println("""
+				Usage: 
+					java -jar build/libs/Kyle-ClassFinder-1.0.jar [class full name] [jars' folder path]
+					java -jar build/libs/Kyle-ClassFinder-1.0.jar [Filename.class] [jars' folder path]
+				""");
 			System.exit(0);
 		}
+		
 		String classFullname = args[0];
-		String classPathname = RegExUtils.replaceAll(classFullname, "\\.", "/");
-		if (!StringUtils.endsWith(classPathname, ".class")) {
-			classPathname += ".class";
-		}
-		System.out.println("Searching class " + classFullname + " ...");
-		Collection<File> jarFiles = FileUtils.listFiles(new File(args[1]), new String[]{"jar"}, true);
-		List<File> tldFiles = new ArrayList<File>();
-		
-		for (File jarFile:jarFiles) {
-			try (ZipFile zipFile = new ZipFile(jarFile)) {
-				Enumeration<? extends ZipEntry> zipEntries = zipFile.entries();
-				while (zipEntries.hasMoreElements()) {
-					ZipEntry zipEntry = zipEntries.nextElement();
-					if (!zipEntry.isDirectory() && zipEntry.getName().endsWith(".class")
-						&& StringUtils.equals(zipEntry.getName(), classPathname)) {
-							
-						System.out.println("Jar found: " + jarFile.getAbsolutePath() + "[" + FileUtils.byteCountToDisplaySize(jarFile.length()) + "]");
-						tldFiles.add(jarFile);
-						break;
-					}
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		String classFilename = classFullname;
+		if (classFilename.endsWith(".class")) {
+			System.out.println("Searching class file " + classFilename + " ...");
 			
-		}
-		
-		/*System.out.println("jars contains tlds: ");
-		for (File jarFile : tldFiles) {
-			System.out.println(jarFile.getName());
-		}*/
-		System.out.println("Class " + classFullname + " found in " + tldFiles.size() + " jars.");
-	}
+			Collection<File> jarFiles = FileUtils.listFiles(new File(args[1]), new String[]{"jar"}, true);
+			List<File> tldFiles = new ArrayList<File>();
+			
+			for (File jarFile:jarFiles) {
+				try (ZipFile zipFile = new ZipFile(jarFile)) {
+					Enumeration<? extends ZipEntry> zipEntries = zipFile.entries();
+					while (zipEntries.hasMoreElements()) {
+						ZipEntry zipEntry = zipEntries.nextElement();
+						if (!zipEntry.isDirectory() && zipEntry.getName().endsWith(classFilename)) {								
+							System.out.println("[" + zipEntry.getName() + "]");
+							System.out.println("Jar found: " + jarFile.getAbsolutePath() + "[" + FileUtils.byteCountToDisplaySize(jarFile.length()) + "]");
+							tldFiles.add(jarFile);
+							break;
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}	
+			}
+			System.out.println("Class name " + classFilename + " found in " + tldFiles.size() + " jars.");
+		} else {		
+			String classPathname = RegExUtils.replaceAll(classFullname, "\\.", "/");
+			System.out.println("Searching class " + classFullname + " ...");
+			Collection<File> jarFiles = FileUtils.listFiles(new File(args[1]), new String[]{"jar"}, true);
+			List<File> tldFiles = new ArrayList<File>();
+			
+			for (File jarFile:jarFiles) {
+				try (ZipFile zipFile = new ZipFile(jarFile)) {
+					Enumeration<? extends ZipEntry> zipEntries = zipFile.entries();
+					while (zipEntries.hasMoreElements()) {
+						ZipEntry zipEntry = zipEntries.nextElement();
+						if (!zipEntry.isDirectory() && zipEntry.getName().endsWith(".class")
+							&& StringUtils.equals(zipEntry.getName(), classPathname)) {
+								
+							System.out.println("Jar found: " + jarFile.getAbsolutePath() + "[" + FileUtils.byteCountToDisplaySize(jarFile.length()) + "]");
+							tldFiles.add(jarFile);
+							break;
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}	
+			}
 
+			/*System.out.println("jars contains tlds: ");
+			for (File jarFile : tldFiles) {
+				System.out.println(jarFile.getName());
+			}*/
+			System.out.println("Class " + classFullname + " found in " + tldFiles.size() + " jars.");
+		}
+	}
 }
